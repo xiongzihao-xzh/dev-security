fd5b0ce95e4df96b195ffcbfcb464a5ee529e5ae
xzh <xiongzihao_xzh@163.com>
2024-04-20 03:09:00 +0800

    feat: Security 基本配置

--- dev-security-component/dev-security-component-auth/src/main/java/com/dev/security/auth/config/SecurityConfig.java
+++ dev-security-component/dev-security-component-auth/src/main/java/com/dev/security/auth/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.dev.security.auth.config;
 
+import com.dev.security.auth.filter.CustomUsernamePasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Spring Security 配置类
@@ -18,9 +22,29 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorizeHttpRequests -> {
-            authorizeHttpRequests.anyRequest().authenticated();
-        });
+        // 关闭不用的过滤器
+        http
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .rememberMe(AbstractHttpConfigurer::disable)
+            .logout(AbstractHttpConfigurer::disable)
+            .sessionManagement(AbstractHttpConfigurer::disable)
+            .csrf(AbstractHttpConfigurer::disable)
+            .requestCache(AbstractHttpConfigurer::disable)
+            .headers(headers -> headers.cacheControl(HeadersConfigurer.CacheControlConfig::disable));
+
+        http
+            .authorizeHttpRequests(authorizeHttpRequests -> {
+                authorizeHttpRequests.anyRequest().authenticated();
+            })
+            .addFilterBefore(
+                new CustomUsernamePasswordAuthenticationFilter(),
+                UsernamePasswordAuthenticationFilter.class
+            );
+        
+        // TODO: 
+        //     - 配置 login 认证成功处理器和失败处理器
+        //     - 配置 ExceptionTranslationFilter
 
         return http.build();
     }
