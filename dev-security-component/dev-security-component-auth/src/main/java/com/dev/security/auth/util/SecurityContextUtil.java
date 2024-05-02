7879240865cd5d3cc88898481ad84f8dbfc498e0
xzh <xiongzihao_xzh@163.com>
2024-05-02 20:10:56 +0800

    init: 初始化 upms 模块

---
+++ dev-security-component/dev-security-component-auth/src/main/java/com/dev/security/auth/util/SecurityContextUtil.java
@@ -0,0 +1,31 @@
+package com.dev.security.auth.util;
+
+/**
+ * SecurityContext 工具类
+ *
+ * @author <a href="mailto:xiongzihao_xzh@163.com">xzh</a>
+ * @date 2024-05-02
+ */
+public class SecurityContextUtil {
+
+    /**
+     * 获取当前上下文用户详细信息
+     */
+    public static Object getUserDetails() {
+        return null;
+    }
+
+    /**
+     * 获取当前上下文用户 ID
+     */
+    public static long getUserId() {
+        return 0L;
+    }
+
+    /**
+     * 获取当前上下文用户名
+     */
+    public static String getUserName() {
+        return null;
+    }
+}
---
+++ dev-security-server/dev-security-server-upms/dev-security-server-upms-biz/src/main/java/com/dev/security/upms/UpmsApplication.java
@@ -0,0 +1,18 @@
+package com.dev.security.upms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO
+ *
+ * @author <a href="mailto:xiongzihao_xzh@163.com">xzh</a>
+ * @date 2024-05-02
+ */
+@SpringBootApplication
+public class UpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpmsApplication.class, args);
+    }
+}
